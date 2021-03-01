77363e44040945d1018709dfce7038985a83a870
irostub <irostub@gmail.com>
2021-03-01 22:28:01 +0900

    Init Commit

---
+++ raflix-backend/src/main/java/com/rainbowflavor/raflix/RaflixApplication.java
@@ -0,0 +1,13 @@
+package com.rainbowflavor.raflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaflixApplication.class, args);
+    }
+
+}
