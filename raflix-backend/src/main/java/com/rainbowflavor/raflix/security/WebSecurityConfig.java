af6c5e373432881c2560539b1792295f85d1c794
irostub <irostub@gmail.com>
2021-03-03 02:49:07 +0900

    구현 configure to http in WebSecurityConfig

--- raflix-backend/src/main/java/com/rainbowflavor/raflix/security/WebSecurityConfig.java
+++ raflix-backend/src/main/java/com/rainbowflavor/raflix/security/WebSecurityConfig.java
@@ -1,11 +1,37 @@
 package com.rainbowflavor.raflix.security;
 
+import com.rainbowflavor.raflix.security.services.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//@EnableGlobalMethodSecurity(prePostEnabled = true) 해당 어노테이션은 차후에 다른 메소드에서 @PreAuthorize 어노테이션을 사용하여 권한에 맞는 사용자가 해당 메소드를 실행할 수 있게 제한한다.
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig {
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsServiceImpl userDetailsService;
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests().anyRequest().permitAll();
+    }
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
