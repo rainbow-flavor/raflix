f0dc6d3ef6dc7d74956f7bf68fe069203b6e0fa9
irostub <irostub@gmail.com>
2021-03-03 22:48:59 +0900

    생성 좋아요 매핑 테이블

---
+++ raflix-backend/src/main/java/com/rainbowflavor/raflix/models/Favorite.java
@@ -0,0 +1,18 @@
+package com.rainbowflavor.raflix.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+}
