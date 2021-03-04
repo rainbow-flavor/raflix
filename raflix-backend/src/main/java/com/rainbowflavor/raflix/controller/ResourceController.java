4d898440dc3ece0f13041ec52f2038969d8d7acc
irostub <irostub@gmail.com>
2021-03-04 23:18:51 +0900

    수정 rest api 매핑

--- raflix-backend/src/main/java/com/rainbowflavor/raflix/controller/ResourceController.java
+++ raflix-backend/src/main/java/com/rainbowflavor/raflix/controller/ResourceController.java
@@ -1,8 +1,12 @@
 package com.rainbowflavor.raflix.controller;
 
+import com.rainbowflavor.raflix.payload.request.MovieRequest;
+import com.rainbowflavor.raflix.payload.response.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins="*", maxAge = 3600)
 public class ResourceController {
 
-    @RequestMapping("/movie/list")
-    public ResponseEntity movie(MovieRequest movieRequest){
-
-
+    //@PreAuthorize() 부현된 권한 확인
+    @GetMapping("/movie/")
+    public ResponseEntity Movie(MovieRequest movieRequest){
+        Long id = movieRequest.getId();
+        /**
+         * TODO: 로그인을 성공적으로 마친 사용자에 대해서만 작동해야하는 요청
+         */
+        return ResponseEntity.ok(new MessageResponse("success get movie request"));
     }
 }
