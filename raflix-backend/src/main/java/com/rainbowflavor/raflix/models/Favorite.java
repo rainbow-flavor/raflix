eff64f3b36994bc42925b43ff62c2764e1b9fb1b
irostub <irostub@gmail.com>
2021-03-06 08:54:26 +0900

    수정 좋아요 엔티티 양방향 매핑을 위한 작업

--- raflix-backend/src/main/java/com/rainbowflavor/raflix/models/Favorite.java
+++ raflix-backend/src/main/java/com/rainbowflavor/raflix/models/Favorite.java
@@ -1,9 +1,17 @@
 package com.rainbowflavor.raflix.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Favorite {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,4 +23,12 @@ public class Favorite {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "movie_id")
     private Movie movie;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
 }
