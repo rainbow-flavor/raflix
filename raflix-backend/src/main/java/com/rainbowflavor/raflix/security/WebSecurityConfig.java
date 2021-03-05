f2f76431eb7b3220bb51ab3a94c3e6656a41f743
irostub <irostub@gmail.com>
2021-03-06 07:50:37 +0900

    수정 WebSecurityConfig 필수 인자 생성자 추가

--- raflix-backend/src/main/java/com/rainbowflavor/raflix/security/WebSecurityConfig.java
+++ raflix-backend/src/main/java/com/rainbowflavor/raflix/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.rainbowflavor.raflix.security;
 
 import com.rainbowflavor.raflix.security.services.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,9 +15,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 //@EnableGlobalMethodSecurity(prePostEnabled = true) 해당 어노테이션은 차후에 다른 메소드에서 @PreAuthorize 어노테이션을 사용하여 권한에 맞는 사용자가 해당 메소드를 실행할 수 있게 제한한다.
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsServiceImpl userDetailsService;
 
