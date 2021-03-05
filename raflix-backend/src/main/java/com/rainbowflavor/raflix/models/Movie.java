41e9cb3542d9112369383e206af81821adc3582c
irostub <irostub@gmail.com>
2021-03-06 08:55:06 +0900

    수정 더 이상 식별자에 대한 setter 를 사용하지 않습니다

--- raflix-backend/src/main/java/com/rainbowflavor/raflix/models/Movie.java
+++ raflix-backend/src/main/java/com/rainbowflavor/raflix/models/Movie.java
@@ -3,24 +3,48 @@ package com.rainbowflavor.raflix.models;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Setter
-@Table(name="movies")
+@Entity
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column
+
     private String title;
-    @Column
-    private String desc;
-    @Column
+
+    private String description;
+
     private String genre;
-    @Column
+
     private String actor;
 
+    @OneToMany(mappedBy = "movie")
+    private List<Favorite> favoriteList = new ArrayList<>();
+
+    public Movie(String title, String description, String genre, String actor) {
+        this.title = title;
+        this.description = description;
+        this.genre = genre;
+        this.actor = actor;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
 }
