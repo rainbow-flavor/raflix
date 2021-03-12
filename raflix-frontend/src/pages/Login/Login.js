9a6898c7c7c35ec89743a258c2a04982bc200621
ranjafunc <seolin9340@gmail.com>
2021-03-12 21:43:27 +0900

    로그인 인증 후 Browse 페이지 렌더 구현

--- raflix-frontend/src/pages/Login/Login.js
+++ raflix-frontend/src/pages/Login/Login.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import HeaderLogo from '../components/HeaderLogo';
+import LoginForm from './LoginForm';
+import Footer from '../components/Footer';
+import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            ddd
+        <div className="login">
+        <header>
+            <Link to="/">
+                <HeaderLogo/>
+            </Link>                
+        </header>
+        <div className="login-bg">
+            <img src="./images/KR-ko-20210222-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="로그인" />
+            <div className="home-background-shadow"></div>
+        </div>
+        <LoginForm />
+        <Footer />
         </div>
     );
 }
