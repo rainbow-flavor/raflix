7820d3f506806f913ab3d2a367d56b5263503101
Ranja Func <seolin9340@gmail.com>
2021-03-15 01:07:26 +0900

    VideoList 슬라이더 구현

--- raflix-frontend/src/pages/Browse/VideoList.js
+++ raflix-frontend/src/pages/Browse/VideoList.js
@@ -1,34 +1,41 @@
-import React, {useState} from 'react';
-import Video from './Video.js';
+import React, { useState } from 'react';
 import { FaAngleRight } from "react-icons/fa";
 import Slider from "react-slick";
+import Video from './Video.js';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import './VideoList.css';
 
 const VideoList = ({ heading, data }) => {    
+    const [click, setClick] = useState(false);   
+     
     const settings = {
-        dots: false,
-        infinite: true,
+        dots: false,        
         speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1
-      };
+        slidesToShow: 5,
+        slidesToScroll: 5,   
+        afterChange : () => setClick(true)       
+    };
+    
     return (
         <div className="video-list">
             <div className="list-name">
                 <h3>
                     {heading}
-                    <span>모두 보기<FaAngleRight/></span>
+                    <span>모두 보기</span>
+                    <b><FaAngleRight/></b>                    
                 </h3>                
             </div>
 
-            <ul className="list">
+            <ul className={`list ${click ? "focus" : ''}`}>
                 <Slider {...settings}>
                     {data.map((info,i) => {
                         const {title, id} = info;
                         return <Video key={id} title={title}/>
-                    })}              
+                    })}                               
                 </Slider>
             </ul>
+            
         </div>
     );
 };
