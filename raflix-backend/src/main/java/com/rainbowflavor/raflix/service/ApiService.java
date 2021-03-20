1c8974febd463b647fc8782c9150aa3357180347
irostub <irostub@gmail.com>
2021-03-20 19:18:55 +0900

    Fix rest api call start with spring boot

--- raflix-backend/src/main/java/com/rainbowflavor/raflix/service/ApiService.java
+++ raflix-backend/src/main/java/com/rainbowflavor/raflix/service/ApiService.java
@@ -1,6 +1,10 @@
 package com.rainbowflavor.raflix.service;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -8,8 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
 import java.util.Map;
-
 @RequiredArgsConstructor
 @Service
 public class ApiService {
@@ -19,6 +24,7 @@ public class ApiService {
     private HttpHeaders header = new HttpHeaders();
     private HttpEntity<?> entity = new HttpEntity<>(header);
 
+    @PostConstruct
     public ResponseEntity get(){
         ResponseEntity<Map> exchange = restTemplate.exchange("https://yts-proxy.now.sh/list_movies.json?sort_by=rating", HttpMethod.GET, entity, Map.class);
         return exchange;
