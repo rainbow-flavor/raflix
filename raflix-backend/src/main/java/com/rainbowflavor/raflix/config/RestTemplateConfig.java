e8ed9116132e731dfcdbd7a1a8e73651133bbcb8
irostub <irostub@gmail.com>
2021-03-22 20:41:46 +0900

    Remove useless comment

--- raflix-backend/src/main/java/com/rainbowflavor/raflix/config/RestTemplateConfig.java
+++ raflix-backend/src/main/java/com/rainbowflavor/raflix/config/RestTemplateConfig.java
@@ -1,27 +1,17 @@
 package com.rainbowflavor.raflix.config;
 
-import ch.qos.logback.classic.pattern.MessageConverter;
-import com.rainbowflavor.raflix.service.ApiService;
-import javassist.Loader;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.JsonbHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
 
 @Configuration
 public class RestTemplateConfig {
-
-/*    @Bean
-    public RestTemplateBuilder restTemplateBuilder(){
-        return new RestTemplateBuilder();
-    }*/
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
